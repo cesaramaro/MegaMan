dfe49554bb79e1405317e4c42797873f0588d875
Cesar Amaro <cesar.amaro@upr.edu>
2018-04-13 01:12:32 -0400

    Two asteroids now show up on the screen at the same time (Level 3)

--- src/rbadia/voidspace/main/Level3State.java
+++ src/rbadia/voidspace/main/Level3State.java
@@ -1,8 +1,13 @@
 package rbadia.voidspace.main;
 
-import java.util.HashMap;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 import rbadia.voidspace.graphics.GraphicsManager;
+import rbadia.voidspace.model.Asteroid;
+import rbadia.voidspace.model.BigBullet;
+import rbadia.voidspace.model.Bullet;
 import rbadia.voidspace.model.Platform;
 import rbadia.voidspace.sounds.SoundManager;
 
@@ -13,7 +18,12 @@ import rbadia.voidspace.sounds.SoundManager;
 public class Level3State extends Level1State {
 
     private static final long serialVersionUID = 3L;
-
+    
+    private ArrayList<Asteroid> asteroidsOnScreen = new ArrayList<Asteroid>(2);
+    
+    protected long secondAsteroidTime;
+    protected Asteroid secondAsteroid;
+    
     // Constructors
     public Level3State(int level, MainFrame frame, GameStatus status, 
             LevelLogic gameLogic, InputHandler inputHandler,
@@ -26,8 +36,32 @@ public class Level3State extends Level1State {
         super.doStart();
         setStartState(GETTING_READY);
         setCurrentState(getStartState());
+
+        newSecondAsteroid(this);
     }
 
+    @Override
+    public void updateScreen() {
+        super.updateScreen();
+        movePlatforms();
+        drawSecondAsteroid();
+        checkBulletSecondAsteroidCollisions();
+        checkBigBulletSecondAsteroidCollisions();
+        checkMegaManSecondAsteroidCollisions();
+        checkSecondAsteroidFloorCollisions();
+    }
+    
+    /*
+     * Method to move the platforms from side to side
+     */
+    public void movePlatforms() {
+        for (Platform platform : platforms) {
+            if (platform.getMaxX() >= SCREEN_WIDTH) platform.reverse = true;
+            else if (platform.getX() <= 0) platform.reverse = false;
+            platform.translate((platform.reverse ? -1 : 1), 0);
+        }
+    }
+    
     @Override
     public Platform[] newPlatforms(int n) {
         platforms = new Platform[n];
@@ -38,19 +72,125 @@ public class Level3State extends Level1State {
                 k = k + 2;
         }
         return platforms;
-    }
-
-    @Override
-    public void updateScreen() {
-        super.updateScreen();
-        movePlatforms(); 
-    }
-
-    public void movePlatforms() {
-        for (Platform platform : platforms) {
-            if (platform.getMaxX() >= SCREEN_WIDTH) platform.reverse = true;
-            else if (platform.getX() <= 0) platform.reverse = false;
-            platform.translate((platform.reverse ? -1 : 1), 0);
+    }   
+    
+    /*
+     * Inefficient way of adding a second asteroid
+     * but there's no time
+     */
+    
+    /**
+     * Create a second asteroid
+     */
+    public Asteroid newSecondAsteroid(Level1State screen) {
+        int xPos = (int) (SCREEN_WIDTH - Asteroid.WIDTH - 50);
+        int yPos = rand.nextInt((int) (SCREEN_HEIGHT - Asteroid.HEIGHT- 32));
+        while(yPos == asteroid.getY()) {
+            yPos = rand.nextInt((int) (SCREEN_HEIGHT - Asteroid.HEIGHT- 32));
         }
+        secondAsteroid = new Asteroid(xPos, yPos);
+        return secondAsteroid;
+    }
+    
+    /*
+     * Draw a second asteroid
+     */
+    protected void drawSecondAsteroid() {
+        Graphics2D g2d = getGraphics2D();
+        GameStatus status = getGameStatus();
+        
+        if ((secondAsteroid.getX() + secondAsteroid.getWidth() > 0)) {
+            secondAsteroid.translate(-secondAsteroid.getSpeed(), 0);
+            getGraphicsManager().drawAsteroid(secondAsteroid, g2d, this); 
+        } else {
+            long currentTime = System.currentTimeMillis();
+            if((currentTime - secondAsteroidTime) > NEW_ASTEROID_DELAY) { 
+                // Draw a new asteroid
+                secondAsteroidTime = currentTime;
+                status.setNewAsteroid(false);
+                secondAsteroid.setLocation((int) (SCREEN_WIDTH - secondAsteroid.getPixelsWide()),
+                        (rand.nextInt((int) (SCREEN_HEIGHT - secondAsteroid.getPixelsTall() - 32))));
+            } else {
+                // Draw explosion
+                getGraphicsManager().drawAsteroidExplosion(asteroidExplosion, g2d, this);
+            }
+        }
+    }
+    
+    /*
+     * Check an asteroid collision with the floor
+     */
+    protected void checkSecondAsteroidFloorCollisions() {
+        for (int i = 0; i < 9; i++) {
+            if (secondAsteroid.intersects(floor[i])) {
+                removeSecondAsteroid(secondAsteroid);
+
+            }
+        }
+    }
+
+    /*
+     * Check MegaMan's collision with the second asteroid
+     */
+    protected void checkMegaManSecondAsteroidCollisions() {
+        GameStatus status = getGameStatus();
+        if (secondAsteroid.intersects(megaMan)) {
+            status.setLivesLeft(status.getLivesLeft() - 1);
+            removeSecondAsteroid(secondAsteroid);
+        }
+    }
+
+    /*
+     * Check collisions between the second asteroid 
+     * and big bullets
+     */
+    protected void checkBigBulletSecondAsteroidCollisions() {
+        GameStatus status = getGameStatus();
+        for (int i = 0; i < bigBullets.size(); i++) {
+            BigBullet bigBullet = bigBullets.get(i);
+            if (secondAsteroid.intersects(bigBullet)) {
+                // increase asteroids destroyed count
+                status.setAsteroidsDestroyed(status.getAsteroidsDestroyed() + 100);
+                removeSecondAsteroid(secondAsteroid);
+                damage = 0;
+            }
+        }
+    }
+
+    /*
+     * Check collisions between the second asteroid
+     * and regular-sized bullets
+     */
+    protected void checkBulletSecondAsteroidCollisions() {
+        GameStatus status = getGameStatus();
+        for(int i = 0; i < bullets.size(); i++){
+            Bullet bullet = bullets.get(i);
+            if (secondAsteroid.intersects(bullet)){
+                // increase asteroids destroyed count
+                status.setAsteroidsDestroyed(status.getAsteroidsDestroyed() + 100);
+                removeSecondAsteroid(secondAsteroid);
+                levelAsteroidsDestroyed++;
+                damage = 0;
+                // remove bullet
+                bullets.remove(i);
+                break;
+            }
+        }
+    }
+    
+    /*
+     * Remove the second Asteroid
+     */
+    public void removeSecondAsteroid(Asteroid asteroid) {
+        asteroidExplosion = new Rectangle(
+                asteroid.x,
+                asteroid.y,
+                asteroid.getPixelsWide(),
+                asteroid.getPixelsTall());
+        asteroid.setLocation(-asteroid.getPixelsWide(), -asteroid.getPixelsTall());
+        this.getGameStatus().setNewAsteroid(true);
+        secondAsteroidTime = System.currentTimeMillis();
+        // Play asteroid explosion sound
+        this.getSoundManager().playAsteroidExplosionSound();
     }
 }
